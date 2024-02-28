a55085f3cb034b3aec0df75850ca7aa07e65dcb2
Youssef Emad <usf3md@gmail.com>
2024-02-28 12:50:13 +0200

    Revert "Add Transaction Cant Read Query Parameter Error Fix"
    This reverts commit b2999f3d253920388c40abfbb828a6ad081e8fdf.

--- app/cashflow/components/AddTransactionForm.tsx
+++ app/cashflow/components/AddTransactionForm.tsx
@@ -10,21 +10,20 @@ import {
   Textarea,
 } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import schema, { Transaction } from "@/app/api/cashflow/transaction/schema";
 import { FieldValues, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Runner } from "@/app/api/cashflow/runner/schema";
 import { TransactionDetail } from "@/app/api/cashflow/transaction-detail/schema";
 import { getTodaysDate } from "@/services/date";
+import { TransactionDetailService } from "@/services/clients";
+import RunnerContext from "@/app/contexts/RunnerContext";
 
 interface Props {
   openingId: string;
-  currentRunner: Runner;
-  details: TransactionDetail[];
 }
 
-const AddTransactionForm = ({ openingId, currentRunner, details }: Props) => {
+const AddTransactionForm = ({ openingId }: Props) => {
   const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -36,12 +35,19 @@ const AddTransactionForm = ({ openingId, currentRunner, details }: Props) => {
     formState: { errors, isValid },
   } = useForm<Transaction>({ resolver: zodResolver(schema) });
 
+  const [details, setDetails] = useState<TransactionDetail[]>([]);
+  const { runner, setRunner } = useContext(RunnerContext);
+  useEffect(() => {
+    TransactionDetailService.getAll(true).then((res) => {
+      setDetails(res.data);
+    });
+  }, []);
   useEffect(() => {
     register("opening");
     setValue("opening", Number(openingId));
     register("runner");
-    setValue("runner", currentRunner?.id!);
-  }, [openingId, currentRunner?.id!]);
+    setValue("runner", runner?.id!);
+  }, [openingId, runner?.id!]);
 
   const onSubmit = (data: FieldValues) => {
     if (data.transaction_detail)
--- app/cashflow/transaction/add/page.tsx
+++ app/cashflow/transaction/add/page.tsx
@@ -1,30 +1,13 @@
-"use client";
-import { TransactionDetail } from "@/app/api/cashflow/transaction-detail/schema";
 import AddTransactionForm from "../../components/AddTransactionForm";
-import { useContext, useEffect, useState } from "react";
-import RunnerContext from "@/app/contexts/RunnerContext";
-import { TransactionDetailService } from "@/services/clients";
 
 interface Props {
   searchParams: { openingId: string };
 }
 
-const Page = async ({ searchParams: { openingId } }: Props) => {
-  const { runner, setRunner } = useContext(RunnerContext);
-  const [details, setDetails] = useState<TransactionDetail[]>([]);
-  useEffect(() => {
-    TransactionDetailService.getAll(true).then((res) => {
-      setDetails(res.data);
-    });
-  }, []);
-
+const Page = ({ searchParams: { openingId } }: Props) => {
   return (
     <article>
-      <AddTransactionForm
-        openingId={openingId}
-        currentRunner={runner}
-        details={details}
-      />
+      <AddTransactionForm openingId={openingId} />
     </article>
   );
 };
