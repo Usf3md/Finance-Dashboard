b2999f3d253920388c40abfbb828a6ad081e8fdf
Youssef Emad <usf3md@gmail.com>
2024-02-28 12:48:28 +0200

    Add Transaction Cant Read Query Parameter Error Fix

--- app/cashflow/transaction/add/page.tsx
+++ app/cashflow/transaction/add/page.tsx
@@ -1,13 +1,30 @@
+"use client";
+import { TransactionDetail } from "@/app/api/cashflow/transaction-detail/schema";
 import AddTransactionForm from "../../components/AddTransactionForm";
+import { useContext, useEffect, useState } from "react";
+import RunnerContext from "@/app/contexts/RunnerContext";
+import { TransactionDetailService } from "@/services/clients";
 
 interface Props {
   searchParams: { openingId: string };
 }
 
-const Page = ({ searchParams: { openingId } }: Props) => {
+const Page = async ({ searchParams: { openingId } }: Props) => {
+  const { runner, setRunner } = useContext(RunnerContext);
+  const [details, setDetails] = useState<TransactionDetail[]>([]);
+  useEffect(() => {
+    TransactionDetailService.getAll(true).then((res) => {
+      setDetails(res.data);
+    });
+  }, []);
+
   return (
     <article>
-      <AddTransactionForm openingId={openingId} />
+      <AddTransactionForm
+        openingId={openingId}
+        currentRunner={runner}
+        details={details}
+      />
     </article>
   );
 };
